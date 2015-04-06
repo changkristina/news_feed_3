8cf62c4b367c96ce0d9795e938535673dfc75402
Kristina Chang <kristinachang@Kristina-Changs-MacBook-Pro.local>
2015-04-05 23:00:25 -0700

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require("pg");
+var methodOverride = require('method-override');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Refactor connection and query code
-var db = require("./models");
+app.use(methodOverride('_method'));
 
+// ./models is a filepath to all the models.
+// var db points to the folder models 
+var db = require("./models");
+//request to server...
 app.get('/articles', function(req,res) {
-  console.log("GET /articles");
+  //query the Article table of the database...	
+  db.Article.findAll().then(function(articles) {
+  	//dislay the query in this path...and pass the results into the articlesList object.
+  	//console.log(articles);
+  	res.render("articles/index", {articlesList: articles});
+  });
 });
 
 app.get('/articles/new', function(req,res) {
@@ -19,12 +28,50 @@ app.get('/articles/new', function(req,res) {
 });
 
 app.post('/articles', function(req,res) {
-  console.log(req.body);
+  db.Article.create({  //creates an instance of the Article model.
+  	title: req.body.title, 
+  	author: req.body.author, 
+  	content: req.body.content, 
+  	fiction: req.body.fiction})
+  			    .then(function(articles) {
+  				      res.redirect('/articles');
+  	});
+  //console.log(req.body);
 });
 
 app.get('/articles/:id', function(req, res) {
-  console.log(req.body);
-  
+  var articleId = req.params.id;
+  db.Article.find(articleId)
+  			    .then(function(articles) {
+  			    	  res.render('articles/article', {articlesList: articles});
+  			 });
+  //console.log(req.body);
+});
+app.get('/articles/:id/edit', function(req, res) {
+  var articleId = req.params.id;
+  db.Article.find(articleId)
+            .then(function(articles) {
+                res.render('articles/edit', {articlesList: articles});
+          });
+});
+
+app.put('/articles/:id', function(req, res) {
+  var articleId = req.params.id;
+  var title = req.body.title;
+  var author = req.body.author;
+  var content = req.body.content;
+  var fiction = req.body.fiction;
+  db.Article.find(articleId)
+            .then(function(articles) {
+              articles.updateAttributes({
+                title: title,
+                author: author,
+                content: content,
+                fiction: fiction })
+              .then(function(articles) {
+                res.redirect(articleId);
+              });
+            });
 })
 
 app.get('/', function(req,res) {
@@ -42,3 +89,5 @@ app.get('/contact', function(req,res) {
 app.listen(3000, function() {
   console.log('Listening');
 });
+
+
